7f865fd0fbf2b6538b7c9223c47daadaa481dbc7
Angelie <angelie_ramdial@moma.org>
2020-03-02 18:54:49 -0500

    restructured files; added controllers with custom namespace

--- app/public/index.php
+++ app/public/index.php
@@ -1,24 +1,6 @@
 <?php
- // required package namespaces
-use Slim\Factory\AppFactory;
-use DI\Container;
-
-// pull in dependencies so they are available within the project
-require '../vendor/autoload.php';
-
-// dependency container - help instantiate a singleton which is only created once throughout app 
-$container = new Container();
-AppFactory::setContainer($container);
-
-// create slim instance
-$app = AppFactory::create();
-// import middleware
-$middleware = require __DIR__ . '/../middleware/errorMiddleware.php';
-// add global app middleware
-$middleware($app);
-// import routes
-$routes = require __DIR__ . '/../routes/routes.php';
-// add global routes
-$routes($app);
+ // pull in dependencies so they are available within the project
+ require '../vendor/autoload.php';
+ require '../app.php';
 // run app
 $app->run();
