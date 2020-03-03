553cbcbfdd21d9946c53252153e6f536b5caafed
Angelie <angelie_ramdial@moma.org>
2020-03-03 14:51:58 -0500

    added working character + url json response

--- app/src/controllers/charactersController.php
+++ app/src/controllers/charactersController.php
@@ -17,7 +17,7 @@ class CharactersController {
     // return list of characters
     public function listAll(Request $req, Response $res){
         if(!empty($this->link)) { 
-            $sql_characters = $this->link->prepare("SELECT * FROM characters LIMIT 20");
+            $sql_characters = $this->link->prepare("SELECT * FROM characters Order by id ASC LIMIT 20");
             $sql_characters->execute();
             $sql_url = $this->link->prepare("SELECT type, url from urls WHERE characterId = :characterId");
             $characters = array();
@@ -25,28 +25,19 @@ class CharactersController {
             while($row=$sql_characters->fetch(PDO::FETCH_ASSOC)){
                 //store character id
                 $this->characterId = $row['id'];
-                // assign character obj
-                $characters['results'][] = $row;
                 // grab urls matching characterId
                 $sql_url->execute(array('characterId' => $this->characterId));
                 $urls =  array();
                 // retrieve urls
-                while($row=$sql_url->fetch(PDO::FETCH_ASSOC)){
-                    // assign url obj
-                    $url = new stdClass();
-                    $url->type = $row['type'];
-                    $url->url = $row['url'];
-                    // combine url array
-                    array_push($urls, $url);
+                while($url_row=$sql_url->fetch(PDO::FETCH_ASSOC)){
+                    // assign and populate url obj
+                    $urls['urls'][] = $url_row;
                 }
-                $characters['urls'][] = $urls;
-                // array_push($characters, $urls);
+                $container = $row + $urls;
+                  // assign character obj
+                $characters['results'][] = $container;
             }
             echo json_encode($characters);
-            // echo json_encode(array('urls' => $urls)); //---> working urls json
-
-            // echo json_encode($urls);  --> This formats correctly to json
-  
         }
         return $res->withHeader('Content-type', 'application/json')->withStatus(200);
     }
